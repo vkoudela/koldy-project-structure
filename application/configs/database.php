71f05220487a0b114e5ce6106f27f52e99efc085
Vlatko Koudela <vlatko@koudela.org>
2014-07-31 01:01:30 +0200

    - updated docs in configs/database.php - added example of backup_connections in configs/database.php

--- application/configs/database.php
+++ application/configs/database.php
@@ -1,21 +1,23 @@
 <?php
 /**
  * This array contains the list of database adapter settings so query can be executed
- * on any adapter you define here. By default, the first item in the array is always the
- * default parameter because most of the websites will need only one database to work with.
+ * on any adapter you define here.
  * 
- * To be able to work on second database, simply call Db::getAdapter('backup'). If you want
- * to work with other database connection on your model, then use protected static property
- * "$connection", such as:
+ * If you want to work with other database connection on your model, then use protected
+ * static property "$connection", such as:
  * 
  * 		class User extends \Koldy\Db\Model {
- *   		protected static $connection = 'backup';
+ *   		protected static $connection = 'second-connection';
  *   	}
  * 
  * @link http://koldy.net/docs/database/configuration
  */
 return array(
 
+
+	/**
+	 * The first connection is the default. You can get this by calling Db::getAdapter()
+	 */
 	'site' => array(
 		'type' => 'mysql',
 		'host' => 'localhost',
@@ -26,7 +28,13 @@ return array(
 		'charset' => 'utf8'
 	),
 
-	'backup' => array(
+
+	/**
+	 * The second connection you may use. You can get this by calling Db::getAdapter('second-connection')
+	 * 
+	 * @link http://koldy.net/docs/database/configuration#connections
+	 */
+	'second-connection' => array(
 		'type' => 'mysql',
 		'host' => '192.168.1.2',
 		'username' => 'root',
@@ -34,6 +42,47 @@ return array(
 		'database' => 'test',
 		'persistent' => false,
 		'charset' => 'utf8'
+	),
+
+
+	/**
+	 * The connection with backups - if first one fails, then framework will
+	 * keep trying to connect by the order defined in backup_connections until it connects
+	 */
+	'connection-with-backups-example' => array(
+		'type' => 'mysql',
+		'host' => '192.168.1.2',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'test',
+		'persistent' => false,
+		'charset' => 'utf8',
+		'driver_options' => array(
+			// if connection is not opened within 3 seconds, try using backup connection
+			PDO::ATTR_TIMEOUT => 3
+		),
+		'backup_connections' => array(
+			array(
+				'log_error' => false,
+				'type' => 'mysql',
+				'host' => '192.168.1.3',
+				'username' => 'root',
+				'password' => '',
+				'database' => 'test',
+				'persistent' => true,
+				'charset' => 'utf8'
+			),
+			array(
+				'log_error' => true,
+				'type' => 'mysql',
+				'host' => '192.168.1.4',
+				'username' => 'root',
+				'password' => '',
+				'database' => 'test',
+				'persistent' => true,
+				'charset' => 'utf8'
+			)
+		)
 	)
 
 );
